f6b6dbe205a47f257dfa43fc1ddab5b7ad95ec3c
David Arvelo <david@davidarvelo.com>
2014-11-16 23:57:21 -0500

    Add splash screen elements and CSS fontfaces

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 import game from 'game';
 
 var App = {
-    start: function () { console.log('App started!'); },
+    start: function () {
+        game.showScreen('splash-screen');
+        console.log('App started!');
+    },
     game: game
 };
 
