94628ace0fcb9e507bfd5da1fd1c593ab217faed
David Arvelo <david@davidarvelo.com>
2014-11-20 22:51:55 -0500

    Add bind evt handler func to dom utils

--- js/util/dom.js
+++ js/util/dom.js
@@ -1,2 +1,15 @@
 export var $ = document.getElementById.bind(document);
 export var $$ = document.querySelectorAll.bind(document);
+export function bind(element, event, handler) {
+    var elStr = element;
+
+    if (typeof element === 'string') {
+        element = $$(element)[0];
+    }
+
+    if (!element) {
+        throw new Error('Could not addEventListener: element "' + elStr + '" did not exist!');
+    }
+
+    element.addEventListener(event, handler, false);
+}
