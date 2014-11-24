20747458c98893587c4392991547049987970877
David Arvelo <david@davidarvelo.com>
2014-11-23 23:55:28 -0500

    Initial game board module

---
+++ js/board.js
@@ -0,0 +1,52 @@
+import settings from 'settings';
+
+var jewels;
+var cols;
+var rows;
+var baseScore;
+var numJewelTypes;
+
+function randomJewel () {
+    return Math.floor(Math.random() * numJewelTypes);
+}
+
+function fillBoard () {
+    jewels = [];
+
+    for (var x = 0; x < cols; ++x) {
+        jewels[x] = [];
+        for (var y = 0; y < rows; ++y) {
+            jewels[x][y] = randomJewel();
+        }
+    }
+}
+
+function getJewel (x, y) {
+    if (x < 0 || x > cols-1 || y < 0 || y > rows-1) {
+        return -1;
+    }
+
+    return jewels[x][y];
+}
+
+export function print () {
+    var str = '';
+    for (var y = 0; y < rows; ++y) {
+        for (var x = 0; x < cols; ++x) {
+            str += getJewel(x, y);
+        }
+
+        str += '\n';
+    }
+
+    console.log(str);
+}
+
+export function initialize (callback) {
+     cols = settings.cols;
+     rows = settings.rows;
+     baseScore = settings.baseScore;
+     numJewelTypes = settings.numJewelTypes;
+     fillBoard();
+     callback();
+}
