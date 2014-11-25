c2e6dbe5daec5747d20885598e1f427f3f810913
David Arvelo <david@davidarvelo.com>
2014-11-24 19:21:35 -0500

    Add module to get an object's type without typeof

---
+++ js/util/type.js
@@ -0,0 +1,11 @@
+export default function type (obj, guess) {
+    if ((guess === 'Undefined' || typeof guess === undefined) && typeof obj === 'undefined') {
+        return true;
+    }
+
+    if ((guess === 'Null' || guess === null) && obj === null) {
+        return true;
+    }
+
+    return ({}).toString.call(obj).slice(8, -1) === guess;
+}
