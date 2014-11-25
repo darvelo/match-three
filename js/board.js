052ee1ceed4749ba5db81085205881a9cc16d970
David Arvelo <david@davidarvelo.com>
2014-11-24 22:36:59 -0500

    Check whether jewels can be swapped

--- js/board.js
+++ js/board.js
@@ -41,6 +41,63 @@ function fillBoard () {
     }
 }
 
+// returns the number of jewels in the longest chain that includes (x, y)
+function checkChain (x, y) {
+    var type = getJewel(x, y);
+    var up, down, left, right;
+    up = down = left = right = 0;
+
+    // look up
+    while (type === getJewel(x, y - up - 1)) {
+        up++;
+    }
+
+    // look down
+    while (type === getJewel(x, y + down + 1)) {
+        down++;
+    }
+
+    // look left
+    while (type === getJewel(x - left - 1, y)) {
+        left++;
+    }
+
+    // look right
+    while (type === getJewel(x + right + 1, y)) {
+        right++;
+    }
+
+    return Math.max(left + 1 + right, up + 1 + down);
+}
+
+function isAdjacent (x1, y1, x2, y2) {
+    var dx = Math.abs(x1 - x2);
+    var dy = Math.abs(y1 - y2);
+    return (dx + dy === 1);
+}
+
+export function canSwap (x1, y1, x2, y2) {
+    var type1 = getJewel(x1, y1);
+    var type2 = getJewel(x2, y2);
+    var chain;
+
+    if (!isAdjacent(x1, y1, x2, y2)) {
+        return false;
+    }
+
+    // temporarily swap jewels to check for chains
+    jewels[x1][y1] = type2;
+    jewels[x2][y2] = type1;
+
+    chain = checkChain(x1, x2) > 2 || checkChain(x2, y2) > 2;
+
+    // swap back
+    jewels[x1][y1] = type1;
+    jewels[x2][y2] = type2;
+
+    return chain;
+}
+
 export function print () {
     var str = '\n';
     for (var y = 0; y < rows; ++y) {
