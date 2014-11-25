503edf90cd2f97dadfeeba717f11e428872af0a1
David Arvelo <david@davidarvelo.com>
2014-11-24 19:56:00 -0500

    Disallow chains to form while setting up the board

--- js/board.js
+++ js/board.js
@@ -11,23 +11,34 @@ function randomJewel () {
     return Math.floor(Math.random() * numJewelTypes);
 }
 
-function fillBoard () {
-    jewels = [];
-
-    for (var x = 0; x < cols; ++x) {
-        jewels[x] = [];
-        for (var y = 0; y < rows; ++y) {
-            jewels[x][y] = randomJewel();
-        }
-    }
-}
-
 function getJewel (x, y) {
     if (x < 0 || x > cols-1 || y < 0 || y > rows-1) {
         return -1;
     }
 
     return jewels[x][y];
+}
+
+function fillBoard () {
+    var type;
+    jewels = [];
+
+    for (var x = 0; x < cols; ++x) {
+        jewels[x] = [];
+
+        for (var y = 0; y < rows; ++y) {
+            type = randomJewel();
+            // disallow chains to form while setting up the board
+            while ((type === getJewel(x-1, y) &&
+                    type === getJewel(x-2, y)) ||
+                    type === getJewel(x, y-1) &&
+                    type === getJewel(x, y-2)) {
+                type = randomJewel();
+            }
+
+            jewels[x][y] = type;
+        }
+    }
 }
 
 export function print () {
