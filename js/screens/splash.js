d34fbd8c9880383aacf305477bc45d7df133f425
David Arvelo <david@davidarvelo.com>
2014-12-01 16:14:43 -0500

    Add preloader progress bar

--- js/screens/splash.js
+++ js/screens/splash.js
@@ -1,13 +1,31 @@
-import { bind } from 'util/dom';
+import { $, $$, bind } from 'util/dom';
+import { getLoadProgress } from 'util/loader';
 
 var firstRun = true;
+var splashScreen = $('splash-screen');
+var progressBar = $$('.loader', splashScreen)[0];
+var continueButton = $$('.continue', splashScreen)[0];
+
 // avoid circular reference when requiring game module
 var game;
 
+function checkLoadProgress () {
+    var percent = (getLoadProgress() * 100)|0;
+
+    progressBar.setAttribute('value', percent);
+
+    if (percent >= 100) {
+        continueButton.style.display = 'block';
+        bind('#splash-screen', 'click', function () {
+            game.showScreen('main-menu');
+        });
+    } else {
+        setTimeout(checkLoadProgress, 30);
+    }
+}
+
 function setup () {
-    bind('#splash-screen', 'click', function () {
-        game.showScreen('main-menu');
-    });
+    checkLoadProgress();
 }
 
 export function run () {
