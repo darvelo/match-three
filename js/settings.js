14b86a27d00ae0e4849ece94d10dbd7c39578167
David Arvelo <david@davidarvelo.com>
2014-11-30 23:49:40 -0500

    Allow accessing jewelSize from settings object

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,10 @@
+import { $ } from 'util/dom';
+
 export default {
     rows: 8,
     cols: 8,
     baseScore: 100,
     numJewelTypes: 7,
+    // element changes size based on screen size
+    jewelSize: $('jewel-proto').getBoundingClientRect().width
 };
