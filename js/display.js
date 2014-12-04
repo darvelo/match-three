c33b9f0f6c34996d40dbc3854b4a248dc6b62972
David Arvelo <david@davidarvelo.com>
2014-12-03 23:59:55 -0500

    Add background canvas

--- js/display.js
+++ js/display.js
@@ -8,6 +8,31 @@ var cols = settings.cols;
 var jewelSize = settings.jewelSize;
 var firstRun = true;
 
+function createBackground () {
+    var bg = document.createElement('canvas');
+    var ctx = bg.getContext('2d');
+    var x, y;
+
+    bg.classList.add('board-bg');
+    bg.width = cols * jewelSize;
+    bg.height = rows * jewelSize;
+
+    ctx.fillStyle = 'rgba(255,235,255,0.15)';
+
+    for (x = 0; x < cols; ++x) {
+        for (y = 0; y < cols; ++y) {
+            if ((x+y) % 2) {
+                ctx.fillRect(
+                    x * jewelSize, y * jewelSize,
+                    jewelSize, jewelSize
+                );
+            }
+        }
+    }
+
+    return bg;
+}
+
 function setup () {
     var boardElement = $$('#game-screen .game-board')[0];
 
@@ -15,6 +40,7 @@ function setup () {
     canvas.width = cols * jewelSize;
     canvas.height = rows * jewelSize;
 
+    boardElement.appendChild(createBackground());
     boardElement.appendChild(canvas);
 }
 
