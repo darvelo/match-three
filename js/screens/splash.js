2bb7e46c3bf0f6ad25c7450938c683cf12798c78
David Arvelo <david@davidarvelo.com>
2014-12-03 23:58:29 -0500

    Bypass splash-screen if nothing needs to be loaded

--- js/screens/splash.js
+++ js/screens/splash.js
@@ -9,22 +9,32 @@ var continueButton = $$('.continue', splashScreen)[0];
 // avoid circular reference when requiring game module
 var game;
 
+function goToMainMenu () {
+    continueButton.style.display = 'block';
+    bind('#splash-screen', 'click', function () {
+        game.showScreen('main-menu');
+    });
+}
+
 function checkLoadProgress () {
     var percent = (getLoadProgress() * 100)|0;
 
     progressBar.setAttribute('value', percent);
 
     if (percent >= 100) {
-        continueButton.style.display = 'block';
-        bind('#splash-screen', 'click', function () {
-            game.showScreen('main-menu');
-        });
+        goToMainMenu();
     } else {
         setTimeout(checkLoadProgress, 30);
     }
 }
 
 function setup () {
+    if (getLoadProgress() === 0) {
+        progressBar.setAttribute('value', 100);
+        goToMainMenu();
+        return;
+    }
+
     checkLoadProgress();
 }
 
