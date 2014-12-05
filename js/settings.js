55aa168c13a979958d22d7f7a896b77c938d28c2
David Arvelo <david@davidarvelo.com>
2014-12-04 23:47:04 -0500

    Add settings for control actions

--- js/settings.js
+++ js/settings.js
@@ -3,4 +3,15 @@ export default {
     cols: 8,
     baseScore: 100,
     numJewelTypes: 7,
+
+    controls: {
+        KEY_UP: 'moveUp',
+        KEY_LEFT: 'moveLeft',
+        KEY_DOWN: 'moveDown',
+        KEY_RIGHT: 'moveRight',
+        KEY_ENTER: 'selectJewel',
+        KEY_SPACE: 'selectJewel',
+        CLICK: 'selectJewel',
+        TOUCH: 'selectJewel',
+    },
 };
