de117f922c06b21be8d2b493a0f9f98e577a93b5
David Arvelo <david@davidarvelo.com>
2014-12-04 23:43:52 -0500

    Add user input module

---
+++ js/input.js
@@ -0,0 +1,16 @@
+import { $ } from 'util/dom';
+import settings from 'settings';
+
+var inputHandlers;
+
+function bind (action, handler) {
+    // bind a handler function to a game action
+}
+
+function trigger (action) {
+    // trigger a game action
+}
+
+export function initialize () {
+    inputHandlers = {};
+}
