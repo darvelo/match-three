46d096956b4e870345e21f964b096c2826cac6a3
David Arvelo <david@davidarvelo.com>
2014-12-11 23:50:52 -0500

    Add requestAnimationFrame polyfill

---
+++ js/util/rAF.js
@@ -0,0 +1,20 @@
+export var rAF = (function() {
+    var startTime = Date.now();
+    return window.requestAnimationFrame ||
+           window.webkitRequestAnimationFrame ||
+           window.mozRequestAnimationFrame ||
+           function(callback) {
+               return window.setTimeout(
+                   function() {
+                       callback(Date.now() - startTime);
+                   }, 1000 / 60
+               );
+           };
+})();
+
+export var cancelRAF = (function() {
+    return window.cancelRequestAnimationFrame ||
+           window.webkitCancelRequestAnimationFrame ||
+           window.mozCancelRequestAnimationFrame ||
+           window.clearTimeout;
+})();
